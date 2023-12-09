a2d64867b41da82fafacfdf0250c2e97418bea03
Lathiya Jayesh <lathiyajayesh77@gmail.com>
2023-12-09 18:19:05 +0530

    created new native ios project

---
+++ native_ios_app/native_ios_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  native_ios_app
+//
+//  Created by Lathiya Jayesh on 08/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func onTapOpenFlutterScreen(_ sender: Any) {
+        
+        
+    }
+    
+
+}
+
